c324d01595ce6a2e159416e64067eb78b7c2689b
purwar1997 <shubhampurwar511@gmail.com>
2023-02-14 19:51:06 +0530

    added cors package

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import router from './routes/routes';
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({ origin: '*' }));
 app.use(router);
 
 export default app;
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ import config from './config/config';
     const res = await mongoose.connect(config.MONGODB_URL);
     console.log(`Database connection success: ${res.connection.host}`);
 
+    app.on('error', err => {
+      throw err;
+    });
+
     app.listen(config.PORT, () =>
       console.log(`Server is running on http://localhost:${config.PORT}`)
     );
