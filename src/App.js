473a89debc15a4dcdc3458f94dc7fd652e393f33
purwar1997 <shubhampurwar511@gmail.com>
2024-03-06 02:42:34 +0530

    feat: add routing for homepage and cartpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { lazy, Suspense } from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Loader from './components/Loader';
+
+const Home = lazy(() => import('./pages/Home'));
+const Cart = lazy(() => import('./pages/Cart'));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route
+        index
+        element={
+          <Suspense fallback={<Loader />}>
+            <Home />
+          </Suspense>
+        }
+      />
+
+      <Route
+        path='cart'
+        element={
+          <Suspense fallback={<Loader />}>
+            <Cart />
+          </Suspense>
+        }
+      />
+    </Route>
+  )
+);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
