231198389b344ef8c5c83cfa1b5d77b93cfd22d5
purwar1997 <shubhampurwar511@gmail.com>
2024-04-09 14:34:52 +0530

    Dispatch thunks to fetch all products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,63 +1,53 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-
-const products = [
-  {
-    id: '1',
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: '2',
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: '3',
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: '4',
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-];
+import { FaStar } from 'react-icons/fa';
+import { fetchProducts } from '../app/slices/productSlice';
 
 const ProductList = () => {
+  const status = useSelector(state => state.products.status);
+  const products = useSelector(state => state.products.data);
+  const error = useSelector(state => state.products.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchProducts());
+    }
+  }, [status, dispatch]);
+
+  if (status === 'loading') {
+    return <h2>Loading...</h2>;
+  }
+
+  if (status === 'failed') {
+    return <h2>{error}</h2>;
+  }
+
   return (
     <div className='grid grid-cols-3 gap-x-8 gap-y-10'>
       {products.map(product => (
         <Link key={product.id} to={`products/${product.id}`}>
           <div className='group'>
-            <img
-              className='rounded group-hover:opacity-80'
-              src={product.imageSrc}
-              alt={product.imageAlt}
-            />
+            <div className='w-80 h-56 overflow-hidden rounded bg-gray-100'>
+              <img
+                className='object-cover object-center group-hover:opacity-80'
+                src={product.thumbnail}
+                alt={product.title}
+              />
+            </div>
 
             <div className='mt-4 flex justify-between'>
               <div>
-                <h3>{product.name}</h3>
-                <p className='mt-1 text-gray-600'>{product.color}</p>
+                <h3>{product.title}</h3>
+
+                <p className='mt-1 flex items-center gap-2'>
+                  <FaStar className='text-yellow-500' />
+                  <span>{Math.round(product.rating * 10) / 10}</span>
+                </p>
               </div>
 
-              <p className='font-medium'>{product.price}</p>
+              <p className='font-medium'>₹{product.price}</p>
             </div>
           </div>
         </Link>
