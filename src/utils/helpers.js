60bfb628c235bfdf75c00595dc60654a5512c587
purwar1997 <shubhampurwar511@gmail.com>
2024-04-19 23:56:45 +0530

    Create helper function to close Modal when clicked outside

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,9 @@
 export const classNames = (...classes) => classes.filter(Boolean).join(' ');
+
+export const handleClickOutside = (e, closeModal) => {
+  if (e.target === e.currentTarget) {
+    closeModal();
+  }
+
+  e.stopPropagation();
+};
