d1c19ad0d06fa8c7a14ff307bc15dac69a102f0b
purwar1997 <shubhampurwar511@gmail.com>
2024-04-26 02:53:22 +0530

    feat: add thunks and reducers to perform CRUD operations on wishlist slice

---
+++ src/app/slices/wishlistAPI.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { addToCartAPI, updateQuantityAPI } from './cartAPI';
+import { selectCartItemById } from './cartSlice';
+import store from '../store';
+
+const client = axios.create({
+  baseURL: 'http://localhost:8000',
+});
+
+export const fetchWishlistAPI = async userId => {
+  const config = {
+    method: 'get',
+    url: `/wishlist?userId=${userId}`,
+  };
+
+  const response = await client(config);
+  return response.data;
+};
+
+export const addToWishlistAPI = async (product, userId) => {
+  const config = {
+    method: 'post',
+    url: '/wishlist',
+    data: {
+      product,
+      userId,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const response = await client(config);
+  return response.data;
+};
+
+export const removeFromWishlistAPI = async id => {
+  const config = {
+    method: 'delete',
+    url: `/wishlist/${id}`,
+  };
+
+  await client(config);
+  return id;
+};
+
+export const moveToCartAPI = async (id, product, userId) => {
+  let config = {
+    method: 'delete',
+    url: `/wishlist/${id}`,
+  };
+
+  await client(config);
+
+  const itemPresentInCart = selectCartItemById(store.getState(), product.id);
+
+  if (itemPresentInCart) {
+    const response = await updateQuantityAPI(itemPresentInCart.id, itemPresentInCart.quantity + 1);
+
+    return response;
+  } else {
+    const response = await addToCartAPI(product, 1, userId);
+    return response;
+  }
+};
---
+++ src/app/slices/wishlistSlice.js
@@ -0,0 +1,83 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  fetchWishlistAPI,
+  addToWishlistAPI,
+  removeFromWishlistAPI,
+  moveToCartAPI,
+} from './wishlistAPI';
+import { addToCart, updateQuantity, selectCartItemById } from './cartSlice';
+
+export const fetchWishlistAsync = createAsyncThunk('wishlist/fetchWishlist', async userId => {
+  return await fetchWishlistAPI(userId);
+});
+
+export const addToWishlistAsync = createAsyncThunk(
+  'wishlist/addToWishlist',
+  async ({ product, userId }) => {
+    return await addToWishlistAPI(product, userId);
+  }
+);
+
+export const removeFromWishlistAsync = createAsyncThunk('wishlist/removeFromWishlist', async id => {
+  return await removeFromWishlistAPI(id);
+});
+
+export const moveToCartAsync = createAsyncThunk(
+  'wishlist/moveToCart',
+  async ({ id, product, userId }, { dispatch, getState }) => {
+    const cartItem = await moveToCartAPI(id, product, userId);
+    const itemPresentInCart = selectCartItemById(getState(), product.id);
+
+    if (itemPresentInCart) {
+      dispatch(updateQuantity(cartItem));
+    } else {
+      dispatch(addToCart(cartItem));
+    }
+
+    return id;
+  }
+);
+
+const initialState = {
+  status: 'idle',
+  items: [],
+  error: null,
+};
+
+const wishlistSlice = createSlice({
+  name: 'wishlist',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchWishlistAsync.pending, state => {
+        state.status = 'loading';
+      })
+      .addCase(fetchWishlistAsync.fulfilled, (state, action) => {
+        state.status = 'succeded';
+        state.items = action.payload;
+      })
+      .addCase(fetchWishlistAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error;
+      })
+      .addCase(addToWishlistAsync.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+      })
+      .addCase(removeFromWishlistAsync.fulfilled, (state, action) => {
+        const index = state.items.findIndex(item => item.id === action.payload);
+        state.items.splice(index, 1);
+      })
+      .addCase(moveToCartAsync.fulfilled, (state, action) => {
+        const index = state.items.findIndex(item => item.id === action.payload);
+        state.items.splice(index, 1);
+      });
+  },
+});
+
+export const selectWishlistItems = state => state.wishlist.items;
+
+export const selectWishlistItemById = (state, id) =>
+  state.wishlist.items.find(item => item.product.id === id);
+
+export default wishlistSlice.reducer;
