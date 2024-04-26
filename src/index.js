14854197759ebd9b3158861c71cedb7bc67d5144
purwar1997 <shubhampurwar511@gmail.com>
2024-04-26 22:54:27 +0530

    Dispatch fetchLoggedInUserAsync thunk before App component renders

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { fetchLoggedInUserAsync } from './app/slices/userSlice';
 import store from './app/store';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+(async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+  await store.dispatch(fetchLoggedInUserAsync(2)).unwrap();
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+})();
