73936eadf4db037477fcef999365523f3d7306c9
purwar1997 <shubhampurwar511@gmail.com>
2024-05-22 02:13:35 +0530

    feat: allow user to delete all items from cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,15 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchCartAsync, selectCartItems, selectCartCount } from '../app/slices/cartSlice';
+import {
+  fetchCartAsync,
+  selectCartItems,
+  selectCartCount,
+  clearCartAsync,
+} from '../app/slices/cartSlice';
 import { fetchWishlistAsync } from '../app/slices/wishlistSlice';
 import { selectLoggedInUser } from '../app/slices/userSlice';
+import { classNames } from '../utils/helpers';
 import CartItem from '../components/CartItem';
 import EmptyCart from '../pages/EmptyCart';
 import LoadingSpinner from '../components/LoadingSpinner';
@@ -16,12 +22,25 @@ const Cart = () => {
   const user = useSelector(selectLoggedInUser);
   const dispatch = useDispatch();
 
+  const [clearCartStatus, setClearCartStatus] = useState('idle');
+
   useEffect(() => {
     if (user) {
       dispatch(fetchCartAsync(user.id));
       dispatch(fetchWishlistAsync(user.id));
     }
   }, [dispatch, user]);
+
+  const handleClearCart = async () => {
+    try {
+      setClearCartStatus('pending');
+      await dispatch(clearCartAsync(cartItems.map(item => item.id))).unwrap();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setClearCartStatus('idle');
+    }
+  };
 
   if (status === 'idle' || status === 'loading') {
     return <LoadingSpinner />;
@@ -36,46 +55,67 @@ const Cart = () => {
   }
 
   return (
-    <main className='page-height px-12 py-10 flex flex-col items-center gap-10'>
-      <h1 className='text-3xl'>Shopping cart</h1>
+    <main className='page-height px-12 py-10 flex justify-center'>
+      <div className='max-w-3xl w-full'>
+        <header className='flex justify-between items-end'>
+          <h1 className='text-3xl'>Shopping cart</h1>
 
-      <section className='max-w-3xl w-full'>
-        <ul className='divide-y divide-gray-200 border-y border-gray-200'>
-          {cartItems.toReversed().map(item => (
-            <CartItem key={item.id} id={item.id} product={item.product} quantity={item.quantity} />
-          ))}
-        </ul>
+          <button
+            className={classNames(
+              'font-medium text-indigo-500 hover:text-indigo-600',
+              clearCartStatus === 'pending' ? 'cursor-wait' : ''
+            )}
+            onClick={handleClearCart}
+            disabled={clearCartStatus === 'pending'}
+          >
+            Clear cart
+          </button>
+        </header>
 
-        <div className='mt-8'>
-          <div className='flex justify-between items-start'>
-            <div>
-              <p className='text-lg font-medium'>Subtotal ({cartCount} items)</p>
-              <p className='mt-1 text-gray-500 text-sm'>
-                Shipping and taxes will be calculated at checkout.
+        <section className='mt-8'>
+          <ul className='divide-y divide-gray-200 border-y border-gray-200'>
+            {cartItems.toReversed().map(item => (
+              <CartItem
+                key={item.id}
+                id={item.id}
+                product={item.product}
+                quantity={item.quantity}
+              />
+            ))}
+          </ul>
+
+          <div className='mt-8'>
+            <div className='flex justify-between items-start'>
+              <div>
+                <p className='text-lg font-medium'>Subtotal ({cartCount} items)</p>
+                <p className='mt-1 text-gray-500 text-sm'>
+                  Shipping and taxes will be calculated at checkout.
+                </p>
+              </div>
+
+              <p className='text-lg font-medium'>
+                ₹
+                {cartItems.reduce((amount, item) => amount + item.product.price * item.quantity, 0)}
               </p>
             </div>
 
-            <p className='text-lg font-medium'>
-              ₹{cartItems.reduce((amount, item) => amount + item.product.price * item.quantity, 0)}
-            </p>
-          </div>
-
-          <div className='mt-7'>
-            <Link to='/checkout'>
-              <button className='w-full h-11 block bg-indigo-600 rounded-md text-white font-medium hover:bg-indigo-700'>
-                Checkout
-              </button>
-            </Link>
-
-            <p className='mt-5 text-center'>
-              or{' '}
-              <Link className='text-indigo-600 font-medium hover:text-indigo-500' to='/'>
-                Continue Shopping
+            <div className='mt-7'>
+              <Link to='/checkout'>
+                <button className='w-full h-11 block bg-indigo-600 rounded-md text-white font-medium hover:bg-indigo-700'>
+                  Checkout
+                </button>
               </Link>
-            </p>
+
+              <p className='mt-5 text-center'>
+                or{' '}
+                <Link className='text-indigo-600 font-medium hover:text-indigo-500' to='/'>
+                  Continue Shopping
+                </Link>
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </main>
   );
 };
